1e0a9c070408b864c6b545d6eac2b7b3845dde18
Lawrence Caulker <locaulker@gmail.com>
2025-05-19 17:07:20 -0400

    setup menu diplay structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
+import menu from "./data.js"
+import Menu from "./Menu"
 import Title from "./Title"
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu)
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
