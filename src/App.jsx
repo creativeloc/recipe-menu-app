e2a7c66d5033ecc651abbef3f517e7126160242a
creativeloc <creativeloc@yahoo.com>
2025-05-20 01:04:01 -0400

    use the set() function to create unique categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import menu from "./data.js"
 import Menu from "./Menu"
 import Title from "./Title"
 
+// const tempCategories = menu.map((item) => item.category)
+// const tempSet = new Set(tempCategories)
+// const tempItems = ["all", ...tempSet]
+
+// console.log(tempItems)
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))]
+// console.log(allCategories)
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+  console.log(categories)
+
   return (
     <main>
       <section className="menu">
